65229fd20e2ac05356cbdfdf9e6e458c2f8c4a61
Muspi Merol <wjj123345567@icloud.com>
2024-05-16 20:08:02 +0800

    feat: auto reload template in dev environment

--- promplate_recipes/context.py
+++ promplate_recipes/context.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from functools import partial
+from functools import cached_property, partial
 from pathlib import Path
 
 from box import Box
@@ -21,6 +21,35 @@ class BuiltinsLayer(dict):
     def __contains__(self, key):
         return key in get_builtins()
 
+    def __repr__(self):
+        return "{ builtins }"
+
+
+class ComponentsLayer(dict):
+    def __init__(self, path: str | Path, pattern="**/*"):
+        self.path = Path(path)
+        self.pattern = pattern
+
+    if __debug__:
+
+        def __getitem__(self, key: str):
+            try:
+                return DotTemplate.read(next(self.path.glob(f"**/{key}.*")))
+            except StopIteration:
+                raise KeyError(key) from None
+
+    else:
+
+        @cached_property
+        def templates(self):
+            return {i.name.split(".", 1)[0]: DotTemplate.read(i) for i in self.path.glob(self.pattern)}
+
+        def __getitem__(self, key: str):
+            return self.templates[key]
+
+    def __repr__(self):
+        return "{ components }"
+
 
 layers = []
 
@@ -39,5 +68,5 @@ class DotTemplate(Template):
         return await super().arender(make_context(context))
 
 
-def register_components(path: Path, pattern="**/*"):
-    layers.append({i.stem: DotTemplate.read(i) for i in path.glob(pattern)})
+def register_components(path: str | Path, pattern="**/*"):
+    layers.append(ComponentsLayer(path, pattern))
